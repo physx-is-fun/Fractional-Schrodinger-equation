d8e2ce189f2f340aadfa09333aa93b43cfafbf9c
Tari Balázs <taribalazs2020@gmail.com>
2025-08-05 16:21:32 +0200

    New studies added

--- fractional_SPM.py
+++ fractional_SPM.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.special import gamma
 from scipy.optimize import minimize_scalar
 import matplotlib.pyplot as plt
-from scipy.fftpack import fft, fftshift, fftfreq
+from scipy.fftpack import fft, ifft, fftshift, ifftshift, fftfreq
 import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("error")
@@ -32,12 +32,23 @@ effective_mode_diameter=5e-6                                                    
 effective_mode_area=(np.pi/4)*effective_mode_diameter**2                              # Effective mode area [m^2]
 nonlinear_refractive_index=2.7*1e-20                                                  # Nonlinear refractive index [m^2/W] of fused silica @ 800 nm from https://opg.optica.org/oe/fulltext.cfm?uri=oe-27-26-37940&id=424534
 gammaconstant=(2*np.pi*nonlinear_refractive_index)/(wavelength0*effective_mode_area)  # Nonlinear parameter [1/(W*m)]
+beta2=0                                                                     # Convert GVD to s^2/m so everything is in SI units of fused silica @ 800nm
+alpha_dB_per_m=0 
 
 # Some useful parameters
 nonlinear_length=1/(gammaconstant*peak_power)
 
 # Prppagation distance
 z = 4.5 * np.pi * nonlinear_length                          # Propagation distance [m]
+
+# Time and frequency grid
+t = np.linspace(-Time_window/2,Time_window/2,N)                                                                                  
+dt = abs(t[1] - t[0])                                   
+f = fftshift(fftfreq(N,d=dt))
+omega = f * 2 * np.pi 
+
+# spatial step
+dz = z / nsteps 
 
 def getPower(amplitude):
     return np.abs(amplitude) ** 2
@@ -50,7 +61,7 @@ def getSpectrumFromPulse(time,pulse_amplitude):
     spectrum_amplitude=fftshift(fft(pulse_amplitude))*dt # Take FFT and do shift
     return spectrum_amplitude
 
-def mittag_leffler_series(alpha, z, K=110):
+def mittag_leffler_series(alpha, z, K=100):
     result = 0
     for k in range(K):
         result += z**k / gamma(alpha * k + 1)
@@ -62,14 +73,16 @@ def mittag_leffler_array(alpha, arg_array):
 def RHS_NL(A):
     return -1j * gammaconstant * np.abs(A)**2 * A
 
-# Time and frequency grid
-t = np.linspace(-Time_window/2,Time_window/2,N)                                                                                  
-dt = abs(t[1] - t[0])                                   
-f = fftshift(fftfreq(N,d=dt))
-omega = f * 2 * np.pi 
-
-# spatial step
-dz = z / nsteps 
+def ssfm_step(A):
+    dispersion_step = np.exp(1j * (beta2 / 2) * omega**2 * dz)
+    loss_step = np.exp(-(alpha_dB_per_m / 2) * dz)
+    nonlineairity_half_step = np.exp(1j * gammaconstant * np.abs(A)**2 * dz / 2)
+    A *= nonlineairity_half_step
+    A_fft = fftshift(fft(ifftshift(A)))
+    A_fft *= dispersion_step * loss_step
+    A = fftshift(ifft(ifftshift(A_fft)))
+    A *= nonlineairity_half_step
+    return A
 
 # Input chirped Gaussian pulse
 A0_t = chirpedGaussianPulseTime(t,amplitude,duration,chirp)
@@ -82,12 +95,16 @@ A_true = A0_t * mittag_leffler_array(true_alpha, arg)
 '''
 
 # Reference measured spectrum (can be simulated with a known alpha)
-true_alpha = 0.94
+true_alpha = 0.95
 A_spectrum = getSpectrumFromPulse(t,A0_t)
 righthandside = RHS_NL(A0_t)
 A_history = [A0_t]
 A_spectrum_history = [A_spectrum]
 prefactor = dz**true_alpha / gamma(true_alpha + 1)
+
+SSFM_A_history = [A0_t]
+SSFM_A_spectrum = getSpectrumFromPulse(t,A0_t)
+SSFM_A_spectrum_history = [SSFM_A_spectrum]
 
 for n in range(1,nsteps):
     A_next = A_history[n-1] - prefactor * RHS_NL(A_history[n-1] - 0.5 * prefactor * RHS_NL(A_history[n-1]))
@@ -95,18 +112,26 @@ for n in range(1,nsteps):
     A_spectrum_next = getSpectrumFromPulse(t,A_next)
     A_spectrum_history.append(A_spectrum_next)
 
+    SSFM_A = SSFM_A_history[n-1]
+    SSFM_A = ssfm_step(SSFM_A)
+    SSFM_A_history.append(SSFM_A)
+    SSFM_A_spectrum = getSpectrumFromPulse(t,SSFM_A)
+    SSFM_A_spectrum_history.append(SSFM_A_spectrum)
+
     delta = int(round(n*100/nsteps)) - int(round((n-1)*100/nsteps))
     if delta == 1:
         print(str(int(round(n*100/nsteps))) + " % ready")
 
 A_true_fft = A_spectrum_history[-1]
 spec_ref = getPower(A_true_fft)
+A_SSFM_fft = SSFM_A_spectrum_history[-1]
+spec_SSFM = getPower(A_SSFM_fft)
 
 # --- Loss function: spectral L2 distance ---
 def spectral_loss(alpha):
     if not (0.9 < alpha < 1.0):
         return np.inf
-    arg = -1j * gammaconstant * getPower(A0_t) * z ** alpha
+    arg = 1j * gammaconstant * getPower(A0_t) * z ** alpha
     A_model = A0_t * mittag_leffler_array(alpha, arg)
     A_model_fft = getSpectrumFromPulse(t,A_model)
 
@@ -124,13 +149,27 @@ print(f"Estimated alpha: {alpha_est:.4f}")
 alpha_est = true_alpha
 
 # --- Plot comparison ---
-arg_fit = -1j * gammaconstant * getPower(A0_t) * z ** alpha_est
+arg_fit = 1j * gammaconstant * getPower(A0_t) * z ** alpha_est
 A_fit = A0_t * mittag_leffler_array(alpha_est, arg_fit)
 A_fit_fft = getSpectrumFromPulse(t,A_fit)
 
+'''
+myfile = open('results5.txt', 'w')
+for index in range(0,N-1):
+    power1 = getPower(A_SSFM_fft)
+    maximum_power1 = np.max(getPower(A_SSFM_fft))
+    power2 = getPower(A_true_fft)
+    maximum_power2 = np.max(getPower(A_true_fft))
+    power3 = getPower(A_fit_fft)
+    maximum_power3 = np.max(getPower(A_fit_fft))
+    myfile.write(f"{f[index]},{np.array(power1[index]/maximum_power1)},{np.array(power2[index]/maximum_power2)},{np.array(power3[index]/maximum_power3)}\n")
+myfile.close()
+'''
+
 plt.figure(figsize=(10,5))
-plt.plot(omega/omega0, getPower(A_true_fft)/np.max(getPower(A_true_fft)), label=f"Reference α={true_alpha}")
-plt.plot(omega/omega0, getPower(A_fit_fft)/np.max(getPower(A_fit_fft)), label=f"Fit α={alpha_est:.3f}", linestyle='--')
+plt.plot(omega/omega0, getPower(A_true_fft)/np.max(getPower(A_true_fft)), label=f"(MFEM) Reference α={true_alpha}")
+plt.plot(omega/omega0, getPower(A_fit_fft)/np.max(getPower(A_fit_fft)), label=f"(half-analitical) Fit α={alpha_est:.3f}", linestyle='--')
+#plt.plot(omega/omega0, getPower(A_SSFM_fft)/np.max(getPower(A_SSFM_fft)), label=f"(SSFM) α=1.0")
 plt.xlabel("Normalized frequency [a.u.]")
 plt.ylabel("Normalized power spectral density [a.u.]")
 plt.title("Spectral Matching for α Estimation")
