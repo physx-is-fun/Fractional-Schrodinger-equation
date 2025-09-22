869147f5472d606829dc4b1d09c5f4267506e19b
Tari Balázs <taribalazs2020@gmail.com>
2025-09-22 08:05:52 +0200

    Computer-aided fitting with spectrum added

--- SSFM_HCF_simulation/libraries2.py
+++ SSFM_HCF_simulation/libraries2.py
@@ -1,7 +1,7 @@
 #libraries2.py
 import numpy as np
 from scipy.special import gamma
-from scipy.optimize import minimize
+from scipy.optimize import minimize_scalar
 from scipy.interpolate import interp1d
 from scipy.signal import fftconvolve
 import matplotlib.pyplot as plt
@@ -11,5 +11,6 @@ from matplotlib.widgets import Slider
 from matplotlib.widgets import Button
 from matplotlib.widgets import CheckButtons
 from scipy.fftpack import fft, ifft, fftshift, ifftshift, fftfreq
+import pandas as pd
 import warnings
 warnings.filterwarnings("error")
